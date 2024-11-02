0cd9ffd8ec3d730895129de38341761fd1d1ae3f
SruthiHareram16 <57579455+SruthiHareram16@users.noreply.github.com>
2024-11-02 00:02:36 -0600

    final

--- batch_aggregation.py
+++ batch_aggregation.py
@@ -4,45 +4,49 @@ import shutil
 import os
 import glob
 
-# Create a Spark session
-spark = SparkSession.builder \
-    .appName("Batch_aggregation.py") \
-    .getOrCreate()
-#Read CSV
-df = spark.read.csv("./Input/batch_agg_input.csv", header=True, inferSchema=True)
+def main():
+    # Create a Spark session
+    spark = SparkSession.builder \
+        .appName("Batch_aggregation.py") \
+        .getOrCreate()
+    #Read CSV
+    df = spark.read.csv("./Input/batch_agg_input.csv", header=True, inferSchema=True)
 
-#Formating
-df = df.withColumn("measurement_type",upper(col("measurement_type")))\
-    .withColumn("date", to_date(col("timestamp")))
-#df.show()
+    #Formating
+    df = df.withColumn("measurement_type",upper(col("measurement_type")))\
+        .withColumn("date", to_date(col("timestamp")))
+    #df.show()
 
-# Aggregating at day level
-aggregated_df = df.groupBy(
-    col("date"),
-    col("measurement_type")
-).agg(
-    round(avg("value"),2).alias("average_value"),
-    min("value").alias("Min Value for the Day"),
-    max("value").alias("Max value for the Day")
-).orderBy(col("date"),col("measurement_type"))
-
-
-#aggregated_df.show()
-
-# writing to csv
-spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
-aggregated_df.write.mode('overwrite').csv("./Output",header=True)
-
-# renaming the file
-
-for filename in os.listdir("./Output"):
-    if filename.startswith("part-"):  # Check if it is a part file
-        old_file_path = os.path.join("./Output", filename)
-        new_file_path = os.path.join("./Output", "batch_agg_output.csv")
-
-        # Move (rename) the file
-        shutil.move(old_file_path, new_file_path)
+    # Aggregating at day level
+    aggregated_df = df.groupBy(
+        col("date"),
+        col("measurement_type")
+    ).agg(
+        round(avg("value"),2).alias("average_value"),
+        min("value").alias("Min Value for the Day"),
+        max("value").alias("Max value for the Day")
+    ).orderBy(col("date"),col("measurement_type"))
 
 
-# Stop the Spark session
-spark.stop()
+    #aggregated_df.show()
+
+    # writing to csv
+    spark.conf.set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+    aggregated_df.write.mode('overwrite').csv("./Output",header=True)
+
+    # renaming the file
+
+    for filename in os.listdir("./Output"):
+        if filename.startswith("part-"):  # Check if it is a part file
+            old_file_path = os.path.join("./Output", filename)
+            new_file_path = os.path.join("./Output", "batch_agg_output.csv")
+
+            # Move (rename) the file
+            shutil.move(old_file_path, new_file_path)
+            
+
+    # Stop the Spark session
+    spark.stop()
+
+if __name__ == '__main__':
+  main()
